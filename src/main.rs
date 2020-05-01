2f9fd7566ccb4596f90144eb99fc625da851a7e4
boio <xorofficial@gmail.com>
2020-05-01 17:19:06 +0200

    port arg

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ extern crate pct_str;
 
 use rocket::config::{Config, Environment};
 use rocket_contrib::json::{Json, JsonValue};
+use std::env;
 
 mod collection;
 mod index;
@@ -25,7 +26,18 @@ fn main() {
   let mut config = Config::build(Environment::Production)
     .unwrap();
 
+  let args: Vec<String> = env::args().collect();
+
   config.port = 7999;
+
+  for (i, arg) in args.iter().enumerate() {
+    if arg.cmp(&String::from("--port")) == std::cmp::Ordering::Equal {
+      let port_num = args[i + 1].parse();
+      if !port_num.is_err() {
+        config.port = port_num.unwrap();
+      }
+    }
+  }
 
   let app = rocket::custom(config);
 
